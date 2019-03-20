b3cb10e43347fedaaea236e01b7a4d0973864121
NorbiRm <norbertoreyesmun@gmail.com>
2019-03-20 08:16:58 -0600

    Initial commit

---
+++ app/src/main/java/com/example/colmilloapp/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.colmilloapp
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent) // Go, go, go
+            finish() // kill this activity
+        }, 3000) // time in milliseconds
+    }
+}
